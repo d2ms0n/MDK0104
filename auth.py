8f80eee6f351200e1a33589a5b4cf91a3a9378a0
d1ms0n <d2ms0n@gmail.com>
2025-05-29 13:39:24 +0500

    Добавлен фронтенд для пользователей

--- auth.py
+++ auth.py
@@ -37,7 +37,7 @@ class AuthService:
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
         else:
-            expire = datetime.utcnow() + timedelta(minutes=15)
+            expire = datetime.utcnow() + timedelta(minutes=65)
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt
